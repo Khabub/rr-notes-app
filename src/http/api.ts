2fe6a4c95ddb419234c4fe9fe0c92a43a52e07ea
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-22 16:03:55 +0200

    xsrf token

--- src/http/api.ts
+++ src/http/api.ts
@@ -2,17 +2,8 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,  
-});
-
-api.interceptors.request.use((config) => {
-  const csrfToken = localStorage.getItem("csrf-token");
-  if (csrfToken) {
-    config.headers["X-XSRF-TOKEN"] = csrfToken;
-  }
-  return config;
 });
 
 export default api;
