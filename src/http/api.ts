a1c3cf827c408146f4e1b472ad6cb427b1aff431
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-22 16:06:00 +0200

    xsrf token

--- src/http/api.ts
+++ src/http/api.ts
@@ -6,4 +6,20 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,  
 });
 
+// Function to retrieve CSRF token from cookies
+const getCsrfToken = () => {
+  const name = "XSRF-TOKEN=";
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name)) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return null;
+};
+
+// Set default headers including the CSRF token
+api.defaults.headers.common["X-XSRF-TOKEN"] = getCsrfToken();
+
 export default api;
