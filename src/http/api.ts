5db6af0c934f719df8b86a22382f092c91870ffc
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-23 13:28:19 +0200

    xsrf token

--- src/http/api.ts
+++ src/http/api.ts
@@ -2,24 +2,12 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+axios.defaults.headers.common["X-CSRF-TOKEN"] = document
+  .querySelector('meta[name="csrf-token"]')!
+  .getAttribute("content");
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,  
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
-
-// Function to retrieve CSRF token from cookies
-const getCsrfToken = () => {
-  const name = "XSRF-TOKEN=";
-  const cookies = document.cookie.split(";");
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    if (cookie.startsWith(name)) {
-      return cookie.substring(name.length, cookie.length);
-    }
-  }
-  return null;
-};
-
-// Set default headers including the CSRF token
-api.defaults.headers.common["X-XSRF-TOKEN"] = getCsrfToken();
 
 export default api;
